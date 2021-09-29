3cb28fde7a9ba1f620898e4e3ac148e2fe0adacc
McGluckerson <90981165+McGluckerson@users.noreply.github.com>
2021-09-29 18:54:40 +0000

    battleship int detection fix
    - fixed error occurring in battleship when inputting a non int. fixed using the same int detection as casino.

--- battleship.py
+++ battleship.py
@@ -26,23 +26,30 @@ def battleship(user):
     print("you have " + str(turns) + " turns left")
     print_board(board)
 
-    guess_row = int(input("guess row: ")) - 1
-    guess_col = int(input("guess col: ")) - 1
+    guess_row = input("guess row: ").strip()
+    guess_col = input("guess col: ").strip()
 
-    if guess_row == ship_row and guess_col == ship_col:
-      print("Hit!")
-      break
-    elif guess_row not in range(0, 5) or ship_col not in range(0,5):
-      print("thats not on the board!")
-    elif board[guess_row][guess_col] == "X":
-      print("you already guessed there")
-      OS.on_enter()
+    if guess_row.isnumeric() and guess_col.isnumeric():
+      guess_row = int(guess_row) - 1
+      guess_col = int(guess_col) - 1
+
+      if guess_row == ship_row and guess_col ==  ship_col:
+        print("Hit!")
+        break
+      elif guess_row not in range(0, 5) or ship_col not in range(0,5):
+        print("thats not on the board!")
+      elif board[guess_row][guess_col] == "X":
+        print("you already guessed there")
+        OS.on_enter()
+      else:
+        print("miss!")
+        board[guess_row][guess_col] = "X"
+        OS.on_enter()
+
+      turns -= 1
     else:
-      print("miss!")
-      board[guess_row][guess_col] = "X"
+      print("guess must be an integer")
       OS.on_enter()
-
-    turns -= 1
   else:
     print("no more turns")
     if input("Play again? ").strip().lower() in OS.yes_words:
